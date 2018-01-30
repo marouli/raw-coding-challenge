6d033cf347dea80ee9be10e5452d1154d9c29050
marouli <maroulisid@gmail.com>
2018-01-30 20:06:50 +0100

    implemented get hello-world

--- app/index.js
+++ app/index.js
@@ -1,18 +1,26 @@
-const http = require('http')
-const port = 3000
+const http = require('http');
+const port = 3000;
 
 const requestHandler = (request, response) => {
-  // We end our request here; please customise this to your own liking!
-  response.end('Hello Node.js Server!')
-}
+// We end our request here; please customise this to your own liking!;
 
+  let url = request.url;
+  let method = request.method;
 
-const server = http.createServer(requestHandler)
+  if(url === '/hello-world' && method === 'GET') {
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.write(JSON.stringify({"hello": "world"}));
+    response.end();
+  }
+    response.end('Hello Node.js Server!');
+  };
+
+const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
   if (err)
-    return console.log('something bad happened', err)
-})
+    return console.log('something bad happened', err);
+});
 
 // You need to export your application in order to be able to pass the tests!
 module.exports = server;
