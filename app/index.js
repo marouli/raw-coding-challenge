682219130ec77c0eb006e9d0a3cd3643a73617ed
marouli <maroulisid@gmail.com>
2018-01-31 13:42:44 +0100

    implemented get 401 status code

--- app/index.js
+++ app/index.js
@@ -3,23 +3,28 @@ const port = 3000;
 
 const requestHandler = (request, response) => {
 // We end our request here; please customise this to your own liking!;
+    let url = request.url;
+    // let method = request.method;
 
-  let url = request.url;
-  let method = request.method;
+    if (url === '/hello-world') {
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.write(JSON.stringify({"hello": "world"}));
+        response.end();
+    }
 
-  if(url === '/hello-world' && method === 'GET') {
-    response.writeHead(200, {'Content-Type': 'application/json'});
-    response.write(JSON.stringify({"hello": "world"}));
-    response.end();
-  }
+    if (url === '/hello-to-admins-only') {
+        response.statusCode = 401;
+        response.end();
+    }
+
     response.end('Hello Node.js Server!');
-  };
+};
 
 const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
-  if (err)
-    return console.log('something bad happened', err);
+    if (err)
+        return console.log('something bad happened', err);
 });
 
 // You need to export your application in order to be able to pass the tests!
